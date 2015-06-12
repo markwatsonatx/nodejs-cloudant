35d1607bab87c559a515b4a1f88b86670dea0c1b
Jason Smith (iMac) <jason.h.smith@gmail.com>
2015-06-12 13:10:30 +0700

    XXX Fri Jun 12 13:10:30 ICT 2015

--- tests/api.js
+++ tests/api.js
@@ -54,3 +54,43 @@ describe('Initialization', function() {
     });
   });
 });
+
+//it('pings the server', function(assert) {
+//  var client = Cloudant({account:'nodejs'});
+//  client.request('', function (er, body, headers) {
+//    assert.equal(er, null, 'Ping Cloudant');
+//    assert.equal(headers.statusCode, 200, 'Cloudant ping status OK');
+//    assert.equal(body.couchdb, 'Welcome', 'Welcome message from Cloudant ping');
+//    assert.ok(body.version, 'Cloudant version string');
+//    assert.end();
+//  });
+//});
+//
+//it('connects with a URL', function(assert) {
+//  var client = Cloudant({account:'http//nodejs.cloudant.com/'});
+//  client.request('', function (er, body, headers) {
+//    assert.equal(er, null, 'Ping Cloudant');
+//    assert.equal(headers.statusCode, 200, 'Cloudant ping status OK');
+//    assert.equal(body.couchdb, 'Welcome', 'Welcome message from Cloudant ping');
+//    assert.ok(body.version, 'Cloudant version string');
+//    assert.end();
+//  });
+//});
+//
+//it('connects with authentication', function(assert) {
+//  var client = Cloudant({account:'nodejs', password:helpers.cloudant_pw()});
+//  client.request('_session', function (er, body, headers) {
+//    assert.equal(er, null, 'Authenticated session check');
+//    assert.equal(headers.statusCode, 200, 'Cloudant session check status OK');
+//    assert.ok(body.ok, 'Session check OK');
+//
+//    var userCtx = body.userCtx || {};
+//    var name = userCtx.name || '';
+//    var roles = userCtx.roles || {};
+//    assert.equal(name, 'nodejs', 'Good authentication as Cloudant user');
+//    assert.ok(~ roles.indexOf('_admin'), 'Admin role');
+//    assert.ok(~ roles.indexOf('_reader'), 'Reader role');
+//    assert.ok(~ roles.indexOf('_writer'), 'Writer role');
+//    assert.end();
+//  });
+//});
